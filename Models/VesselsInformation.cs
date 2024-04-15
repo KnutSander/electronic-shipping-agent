1b9135a452b958fa202b371a24fc17b031b8073f
KnutSander <kb19109@essex.ac.uk>
2024-04-15 18:37:11 +0200

    Updated deserialisation to be case insensitive to maintain naming standards

--- Models/VesselsInformation.cs
+++ Models/VesselsInformation.cs
@@ -4,17 +4,17 @@ namespace electronic_shipping_agent.Models
     public class VesselsInformation
     {
         public class Dimensions {
-            public int width { get; set;}
-            public int height { get; set;}
+            public int Width { get; set;}
+            public int Height { get; set;}
         }
 
         public class Fleet {
-            public Dimensions singleShipDimensions { get; set;}
-            public string shipDesignation { get; set;}
-            public int shipCount { get; set;}
+            public Dimensions SingleShipDimensions { get; set;}
+            public string ShipDesignation { get; set;}
+            public int ShipCount { get; set;}
         }
 
-        public Dimensions anchorageSize { get; set;}
-        public Fleet[] fleets { get; set;}
+        public Dimensions AnchorageSize { get; set;}
+        public Fleet[] Fleets { get; set;}
     }
 }
--- Services/VesselsInformationService.cs
+++ Services/VesselsInformationService.cs
@@ -1,16 +1,19 @@
 
 using System.Text.Json;
 using electronic_shipping_agent.Models;
+using Microsoft.VisualBasic;
 
 namespace electronic_shipping_agent.Services
 {
     public class VesselsInformationService
     {
+        // Option to disable deserialisation from being case sensitive
+        private readonly JsonSerializerOptions _options = new JsonSerializerOptions {PropertyNameCaseInsensitive = true};
         public VesselsInformation DecodeVesselsInformation (string jsonData)
         {
             try 
             {
-                var vesselsInformation = JsonSerializer.Deserialize<VesselsInformation>(jsonData)!;
+                var vesselsInformation = JsonSerializer.Deserialize<VesselsInformation>(jsonData, _options);
                 return vesselsInformation;
             }
             catch (Exception ex)
